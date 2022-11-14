72f18f9f1e812fadd32f476cc0c9baab7d401136
Jose Rene Fuentes <jfetjf@hotmail.com>
2022-11-14 16:41:54 +0100

    Adding templates/ to the .well-knoww folder

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 app = Flask(__name__)
 
 
-@app.route('/.well-known/did.json')
+@app.route('templates/.well-known/did.json')
 def did():
    print('Request for did.json file received')
-   return render_template('/.well-known/did.json')
+   return render_template('did.json')
    
-@app.route('/.well-known/did-configuration.json')
+@app.route('templates/.well-known/did-configuration.json')
 def didconfiguration():
    print('Request for did-configuration.json file received')
-   return render_template('/.well-known/did-configuration.json')
+   return render_template('did-configuration.json')
 
 @app.route('/')
 def index():
